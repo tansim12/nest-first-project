f3097784b773532fd8f78edf745088a5d7226810
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-26 23:04:22 +0600

    middleware logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,45 @@
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { SongsModule } from "./songs/songs.module";
+import { LoggerMiddleware } from "./common/middleware/logger/logger.middleware";
+import { SongsController } from "./songs/songs.controller";
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes("songs"); // options 1 implement all songs route
+    consumer.apply(LoggerMiddleware).forRoutes(
+      {
+        path: "songs",
+        method: RequestMethod.POST,
+      },
+      {
+        path: "songs",
+        method: RequestMethod.GET,
+      },
+      {
+        path: "songs/:id",
+        method: RequestMethod.GET,
+      },
+      {
+        path: "songs/:id",
+        method: RequestMethod.PUT,
+      },
+      {
+        path: "songs/:id",
+        method: RequestMethod.DELETE,
+      },
+    );  // spesific path and method 
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // this middleware apply for SongsController all routes
+  }
+}
