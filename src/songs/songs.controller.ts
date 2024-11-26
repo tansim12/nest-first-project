51bc658b94648bef301df2bfc8b18d26f921e064
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-26 20:50:12 +0600

    songs mvc

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,7 +1,24 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
 @Controller("songs")
 export class SongsController {
+  @Post()
+  create() {
+    return "Song Crate ";
+  }
+  @Put(":id")
+  update(@Param("id") id) {
+    return `Song Update ${id}`;
+  }
+  @Delete(":id")
+  delete(@Param("id") id) {
+    return `Song Delete ${id}`;
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id) {
+    return `Song Get ${id}`;
+  }
   @Get()
   findAll() {
     return "Find All Songs end points";
