5c212fb1899b052193b8b6b3b7a49f88ce719e22
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-27 18:37:45 +0600

    devConfigService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import { DevConfigService } from "./common/providers/DevConfigService";
 
 @Injectable()
 export class AppService {
+  constructor(
+    private devConfigService: DevConfigService,
+    @Inject("CONFIG") private config: { port: string },
+  ) {}
   getHello(): string {
-    return "Hello World! Tansim Ahmed Tashdid";
+    return `Hello World! Tansim Ahmed Tashdid ${this.devConfigService.getDBHOST()} port is ${this.config.port} `;
   }
 }
